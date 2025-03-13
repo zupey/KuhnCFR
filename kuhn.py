ea233a22833819dd286e0c5e19378e637d6614ba
Linus <linuslee2000@gmail.com>
2025-03-13 13:37:04 +0800

    add utility calc

--- kuhn.py
+++ kuhn.py
@@ -3,6 +3,7 @@ import typing
 from typing import Optional, Dict, List, Tuple
 from itertools import permutations
 from collections import deque, defaultdict
+import random
 
 starting_states = list(permutations("JQK", 2))
 
@@ -80,7 +81,7 @@ class PlayerNode(Node):
         self.children = {
             action: PlayerNode(
                 self,
-                Player.other(player),
+                player.other(),
                 self.next_actions(action),
                 self.cards,
                 self.history + [action],
@@ -127,6 +128,11 @@ class PlayerNode(Node):
 ### Form Treeplex
 
 def dfs(root):
+    """Prints Nodes in DFS Order
+
+    Args:
+        root (Node): Root of Game Tree
+    """
     stack = [root]
     while stack:
         node = stack.pop()
@@ -134,19 +140,40 @@ def dfs(root):
         for child_node in node.children:
             stack.append(child_node)
 
+def generate_leaves(root: Node) -> List[PlayerNode]:
+    stack: List[Node] = [root]
+    res: List[PlayerNode] = []
+    while stack:
+        node = stack.pop()
+        if node.is_terminal_state():
+            res.append(node) # type: ignore
+        for child_node in node.children.values():
+            stack.append(child_node)
+    return res
 
 class TreeplexNode:
-    def __init__(self, card: Cards, history:List[Action]):
-        self.card = card
-        self.history = history
+    def __init__(self, card: Cards, parent: Optional['TreeplexNode'], history:List[Action]):
+        self.card: Cards = card
+        self.history: List[Action] = history
         self.infoset = f"{card}, {history}"
-        self.parent: Optional[TreeplexNode]= None
+        self.parent: Optional[TreeplexNode] = parent
         self.children: List[TreeplexNode]= []
         self.actions: List[Action] = self.get_actions()
         # initialize uniform strategy (behavioural form)
-        self.strategy: Dict[Action, float] = {
-            action: 1/len(self.actions) for action in self.actions
-        }
+        if self.actions:
+            x = random.randint(1, 10)
+            self.behavioural_strategy: Dict[Action, float] = {
+                self.actions[0] : 0.7,
+                self.actions[1] : 0.3
+                # action: 1/len(self.actions) for action in self.actions
+            }
+        self.sequence_form_strategy: Dict[Action, float] = {}
+        for action in self.actions:
+            if self.parent and self.parent.actions:
+                self.sequence_form_strategy[action] = (1/len(self.actions)) * self.parent.behavioural_strategy[self.history[-2]]
+            else:
+                self.sequence_form_strategy[action] = self.behavioural_strategy[action]
+                
 
     def is_terminal_state(self):
         return self.children == []
@@ -154,10 +181,10 @@ class TreeplexNode:
     def __str__(self):
         if self.parent is None:
             return "ROOT"
-        return f"(<{self.infoset}>, strat: {self.strategy})"
+        return f"(<{self.infoset}>, strat: {self.sequence_form_strategy})"
 
     def __repr__(self):
-        return f"(<{self.infoset}>, strat: {self.strategy})"
+        return f"(<{self.infoset}>, strat: {self.sequence_form_strategy})"
 
     def get_actions(self) -> List[Action]:
         """Get Actions at this node
@@ -196,10 +223,12 @@ class TreeplexNode:
 def hash(card, history):
     return f"{card}, {history}"
 
+node_map: Dict[str, TreeplexNode] = {}
+
 def generate_treeplex(player: Player, root: Node):
     stack : List[Node] = [root]
-    treeplex_root = TreeplexNode(Cards.Blank, [])
-    map = {"" : treeplex_root}
+    treeplex_root = TreeplexNode(Cards.Blank, None, [])
+    map = {"Blank" : treeplex_root}
     while stack:
         node = stack.pop()
         if node.player == player and not node.is_terminal_state():
@@ -208,9 +237,9 @@ def generate_treeplex(player: Player, root: Node):
             card, history = node.information_set
             s = hash(card, history)
             if s not in map:
-                treeplex_node = TreeplexNode(card, history)
-                parent = "" if len(history) <= 2 else hash(card, history[:-2])
-                treeplex_node.parent = map[parent]
+                parent = "Blank" if len(history) <= 2 else hash(card, history[:-2])
+                treeplex_node = TreeplexNode(card, map[parent], history)
+                node_map[treeplex_node.infoset] = treeplex_node
                 map[parent].children.append(treeplex_node)
                 map[s] = treeplex_node
         for child_node in node.children.values():
@@ -221,5 +250,26 @@ root = ChanceNode()
 # dfs(root)
 tree1 = generate_treeplex(Player.PLAYER_1, root)
 tree2 = generate_treeplex(Player.PLAYER_2, root)
-tree1.display_tree()
-tree2.display_tree()
+# tree1.display_tree()
+# tree2.display_tree()
+
+leaves: List[PlayerNode] = generate_leaves(root)
+
+# for key,val in node_map.items():
+#     print(key,val)
+
+def calc_utility_game():
+    expected_val = 0
+    for leaf in leaves:
+        if leaf.player == Player.PLAYER_1:
+            val = leaf.evaluate() * node_map[hash(leaf.cards[1], leaf.history[:-1])].sequence_form_strategy[leaf.history[-1]] * node_map[hash(leaf.cards[0], leaf.history[:-2])].sequence_form_strategy[leaf.history[-2]]
+        elif leaf.player == Player.PLAYER_2:
+            val = leaf.evaluate() * node_map[hash(leaf.cards[0], leaf.history[:-1])].sequence_form_strategy[leaf.history[-1]] * node_map[hash(leaf.cards[1], leaf.history[:-2])].sequence_form_strategy[leaf.history[-2]]
+        print(leaf, val)
+        expected_val += val
+    return expected_val
+
+def best_response():
+    pass
+
+print(calc_utility_game())
