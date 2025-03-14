1aa18b172e6d04270c00049058acae9b0c452b6f
Linus <linuslee2000@gmail.com>
2025-03-14 16:12:36 +0800

    complete treeplex impl and computed + checked payoff matrix

--- kuhn.py
+++ kuhn.py
@@ -4,6 +4,9 @@ from typing import Optional, Dict, List, Tuple
 from itertools import permutations
 from collections import deque, defaultdict
 import random
+from dataclasses import dataclass
+from fractions import Fraction
+import numpy as np
 
 starting_states = list(permutations("JQK", 2))
 
@@ -113,11 +116,17 @@ class PlayerNode(Node):
         elif self.cards[0] == "J" or self.cards[1] == "K":
             base = -1
 
-        if (self.history[-2] == Action.CHECK and self.history[-1] == Action.CHECK) or (self.history[-2] == Action.BET and self.history[-1] == Action.FOLD):
+        if (self.history[-2] == Action.CHECK and self.history[-1] == Action.CHECK):
             return base
 
-        if self.history[-2] == Action.BET and self.history[-1] == Action.CALL:
+        elif self.history[-2] == Action.BET and self.history[-1] == Action.CALL:
             return base * 2
+        
+        elif (self.history[-2] == Action.BET and self.history[-1] == Action.FOLD):
+            if self.player == Player.PLAYER_1:
+                return 1
+            elif self.player == Player.PLAYER_2:
+                return -1
 
     def __repr__(self):
         return f"""PlayerNode: History:{self.history}, {self.player}, Cards:{''.join(self.cards)}, Value:{self.evaluate()}"""
@@ -125,156 +134,6 @@ class PlayerNode(Node):
     def __str__(self):
         return f"""PlayerNode: History:{self.history}, {self.player}, Cards:{''.join(self.cards)}, Value:{self.evaluate()}"""
 
-
-### Form Treeplex
-
-def dfs(root):
-    """Prints Nodes in DFS Order
-
-    Args:
-        root (Node): Root of Game Tree
-    """
-    stack = [root]
-    while stack:
-        node = stack.pop()
-        print(node)
-        for child_node in node.children:
-            stack.append(child_node)
-
-def generate_leaves(root: Node) -> List[PlayerNode]:
-    stack: List[Node] = [root]
-    res: List[PlayerNode] = []
-    while stack:
-        node = stack.pop()
-        if node.is_terminal_state():
-            res.append(node) # type: ignore
-        for child_node in node.children.values():
-            stack.append(child_node)
-    return res
-
-class TreeplexNode:
-    def __init__(self, card: Cards, parent: Optional['TreeplexNode'], history:List[Action]):
-        self.card: Cards = card
-        self.history: List[Action] = history
-        self.infoset = f"{card}, {history}"
-        self.parent: Optional[TreeplexNode] = parent
-        self.children: List[TreeplexNode]= []
-        self.actions: List[Action] = self.get_actions()
-        # initialize uniform strategy (behavioural form)
-        if self.actions:
-            x = random.randint(1, 10)
-            self.behavioural_strategy: Dict[Action, float] = {
-                self.actions[0] : 0.7,
-                self.actions[1] : 0.3
-                # action: 1/len(self.actions) for action in self.actions
-            }
-        self.sequence_form_strategy: Dict[Action, float] = {}
-        for action in self.actions:
-            if self.parent and self.parent.actions:
-                self.sequence_form_strategy[action] = (1/len(self.actions)) * self.parent.behavioural_strategy[self.history[-2]]
-            else:
-                self.sequence_form_strategy[action] = self.behavioural_strategy[action]
-                
-
-    def is_terminal_state(self):
-        return self.children == []
-
-    def __str__(self):
-        if self.parent is None:
-            return "ROOT"
-        return f"(<{self.infoset}>, strat: {self.sequence_form_strategy})"
-
-    def __repr__(self):
-        return f"(<{self.infoset}>, strat: {self.sequence_form_strategy})"
-
-    def get_actions(self) -> List[Action]:
-        """Get Actions at this node
-
-        Returns:
-            List[Action]: List of possible actions at this decision node (alphabetical order)
-        """
-        # history = [chance], P1 turn
-        if len(self.history) == 1:
-            return [Action.BET, Action.CHECK]
-        # history = [chance, check/bet] P2 turn
-        if len(self.history) == 2:
-            if self.history[-1] == Action.CHECK:
-                return [Action.BET, Action.CHECK]
-            elif self.history[-1] == Action.BET:
-                return [Action.CALL, Action.FOLD]
-        # history = [chance, check, bet] P1 turn
-        if len(self.history) == 3:
-            return [Action.CALL, Action.FOLD]
-        # catchall at chance node
-        return []
-
-    def display_tree(self) -> None:
-        "Pretty printing of tree"
-        levels = defaultdict(list)
-        queue = deque([self])
-        while queue:
-            node = queue.popleft()
-            levels[len(node.history)].append(node)
-            for child in node.children:
-                queue.append(child)
-        keys = levels.keys()
-        for key in sorted(keys):
-            print(*levels[key])
-    
-def hash(card, history):
-    return f"{card}, {history}"
-
-node_map: Dict[str, TreeplexNode] = {}
-
-def generate_treeplex(player: Player, root: Node):
-    stack : List[Node] = [root]
-    treeplex_root = TreeplexNode(Cards.Blank, None, [])
-    map = {"Blank" : treeplex_root}
-    while stack:
-        node = stack.pop()
-        if node.player == player and not node.is_terminal_state():
-            if node.information_set is None:
-                continue
-            card, history = node.information_set
-            s = hash(card, history)
-            if s not in map:
-                parent = "Blank" if len(history) <= 2 else hash(card, history[:-2])
-                treeplex_node = TreeplexNode(card, map[parent], history)
-                node_map[treeplex_node.infoset] = treeplex_node
-                map[parent].children.append(treeplex_node)
-                map[s] = treeplex_node
-        for child_node in node.children.values():
-            stack.append(child_node)
-    return treeplex_root
-
-root = ChanceNode()
-# dfs(root)
-tree1 = generate_treeplex(Player.PLAYER_1, root)
-tree2 = generate_treeplex(Player.PLAYER_2, root)
-# tree1.display_tree()
-# tree2.display_tree()
-
-leaves: List[PlayerNode] = generate_leaves(root)
-
-# for key,val in node_map.items():
-#     print(key,val)
-
-# Currently does not take into account chance nodes
-def calc_utility_game():
-    expected_val = 0
-    for leaf in leaves:
-        if leaf.player == Player.PLAYER_1:
-            val = leaf.evaluate() * node_map[hash(leaf.cards[1], leaf.history[:-1])].sequence_form_strategy[leaf.history[-1]] * node_map[hash(leaf.cards[0], leaf.history[:-2])].sequence_form_strategy[leaf.history[-2]]
-        elif leaf.player == Player.PLAYER_2:
-            val = leaf.evaluate() * node_map[hash(leaf.cards[0], leaf.history[:-1])].sequence_form_strategy[leaf.history[-1]] * node_map[hash(leaf.cards[1], leaf.history[:-2])].sequence_form_strategy[leaf.history[-2]]
-        print(leaf, val)
-        expected_val += val
-    return expected_val
-
-def best_response():
-    pass
-
-# print(calc_utility_game())
 
 class Infoset:
     def __init__(self, card: Cards, history: List[Action], seq_id_list: List[int], parent_seq_id: int):
@@ -331,11 +190,19 @@ class Treeplex:
                 return infoset
         return None
 
+@dataclass
+class Leaf:
+    value: int
+    seq1_id: int
+    seq2_id: int
+
+root = ChanceNode()
 # Generate List of Sequences in topological order (BFS)
 player1_treeplex = Treeplex([], [Sequence()])
 player2_treeplex = Treeplex([], [Sequence()])
 seen: set[Tuple[Cards, Tuple[Action,...]]] = set()
 queue: deque[Tuple[Node, int, int]] = deque([(root, 0, 0)])
+leaves: List[Leaf] = []
 while queue:
     node, p1_idx, p2_idx = queue.popleft()
     p1parent_seq, p2parent_seq = player1_treeplex.sequences[p1_idx], player2_treeplex.sequences[p2_idx]
@@ -358,6 +225,8 @@ while queue:
                 children.append(child)
             infoset = Infoset(node.card, node.history, children_seq, p1_idx)
             player1_treeplex.add_infoset(infoset)
+            for child_seq in children_seq:
+                player1_treeplex.sequences[child_seq].add_parent(infoset)
         elif node.player == Player.PLAYER_2:
             # Generate Children Sequences
             for action in node.children.keys():
@@ -366,13 +235,15 @@ while queue:
                 children.append(child)
             infoset = Infoset(node.card, node.history, children_seq, p2_idx)
             player2_treeplex.add_infoset(infoset)
+            for child_seq in children_seq:
+                player2_treeplex.sequences[child_seq].add_parent(infoset)
 
         # add infoset into seen so we process each infoset once
         seen.add(node.information_set)
     
     # Process leaf logics
-    if node.is_terminal_state():
-        pass
+    if node.is_terminal_state() and isinstance(node, PlayerNode):
+        leaves.append(Leaf(node.evaluate(), p1_idx, p2_idx))
     if isinstance(node, ChanceNode):
         for action, child in node.children.items():
                 queue.append((child, p1_idx, p2_idx))
@@ -389,7 +260,29 @@ while queue:
                 queue.append((child, p1_idx, id))
 
 
-print(player1_treeplex.infosets)
-# print(player1_treeplex.sequences)
-print(player2_treeplex.infosets)
-# print(player2_treeplex.sequences)
+# print(player1_treeplex.infosets)
+print(player1_treeplex.sequences)
+# print(player2_treeplex.infosets)
+print(player2_treeplex.sequences)
+# print(leaves)
+
+payoff_matrix: List[List[float]] = [[0. for _ in range(len(player2_treeplex.sequences))] for _ in range(len(player1_treeplex.sequences))]
+print(len(leaves))
+for leaf in leaves:
+    seq1 = player1_treeplex.sequences[leaf.seq1_id]
+    seq2 = player2_treeplex.sequences[leaf.seq2_id]
+    # print(seq1, seq1.infoset.card, seq2, seq2.infoset.card, leaf.value)
+    payoff_matrix[leaf.seq1_id][leaf.seq2_id] += leaf.value / 6
+
+
+print(np.array(payoff_matrix))
+
+
+"""
+TODO
+1. Initialize strategy for each seq
+2. Enforce Checks (strategy between 0 and 1 + sum to parent)
+3. Calculate xPy (utility)
+4. Calculate best reponse by backward induction
+5.++ CFR
+"""
